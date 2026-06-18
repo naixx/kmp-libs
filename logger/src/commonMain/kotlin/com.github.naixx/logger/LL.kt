c20e06ccc99590b137358345951174997bd534fc
naixx <r.chekan@gmail.com>
2026-06-18 10:08:26 +0300

    Nicer logger output

--- logger/src/commonMain/kotlin/com.github.naixx/logger/LL.kt
+++ logger/src/commonMain/kotlin/com.github.naixx/logger/LL.kt
@@ -16,31 +16,31 @@ object LL {
 
     fun e(a: Any?) {
         val source = getMethodAndLine()
-        Napier.e(tag = source?.tag) { a?.toString() + (source?.let { "--->(${source.line}" } ?: "") }
+        Napier.e(tag = source?.tag) { a?.toString() + (source?.let { " (${source.line}" } ?: "") }
     }
 
     fun e(throwable: Throwable?, a: Any?) {
         val source = getMethodAndLine()
-        Napier.e(tag = source?.tag, throwable = throwable) { a?.toString() + (source?.let { "--->(${source.line}" } ?: "") }
+        Napier.e(tag = source?.tag, throwable = throwable) { a?.toString() + (source?.let { " (${source.line}" } ?: "") }
     }
 
     fun d(a: Any?) {
         val source = getMethodAndLine()
-        Napier.d(tag = source?.tag) { a?.toString() + (source?.let { "--->(${source.line}" } ?: "") }
+        Napier.d(tag = source?.tag) { a?.toString() + (source?.let { " (${source.line}" } ?: "") }
     }
 
     fun w(a: Any?) {
         val source = getMethodAndLine()
-        Napier.w(tag = source?.tag) { a?.toString() + (source?.let { "--->(${source.line}" } ?: "") }
+        Napier.w(tag = source?.tag) { a?.toString() + (source?.let { " (${source.line}" } ?: "") }
     }
 
     fun w(throwable: Throwable?, a: Any?) {
         val source = getMethodAndLine()
-        Napier.w(tag = source?.tag, throwable = throwable) { a?.toString() + (source?.let { "--->(${source.line}" } ?: "") }
+        Napier.w(tag = source?.tag, throwable = throwable) { a?.toString() + (source?.let { " (${source.line}" } ?: "") }
     }
 
     fun i(a: Any?) {
         val source = getMethodAndLine()
-        Napier.i(tag = source?.tag) { a?.toString() + (source?.let { "--->(${source.line}" } ?: "") }
+        Napier.i(tag = source?.tag) { a?.toString() + (source?.let { " (${source.line}" } ?: "") }
     }
 }
